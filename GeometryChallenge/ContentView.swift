b4a998e0f6787f6644f9c2f1ea42f82de6aa0dad
Víctor Ávila <victoraavila2000@gmail.com>
2024-09-28 21:20:47 -0300

    Challenge 1 completed

--- GeometryChallenge/ContentView.swift
+++ GeometryChallenge/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { proxy in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(colors[index % 7])
+                            .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .opacity(proxy.frame(in: .global).minY / 200)
+                    }
+                    .frame(height: 40)
+                }
+            }
         }
-        .padding()
     }
 }
 
