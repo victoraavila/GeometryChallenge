efd03439a335962772e29cd03dfdd06aa667b3a1
Víctor Ávila <victoraavila2000@gmail.com>
2024-09-28 21:02:02 -0300

    Initial Commit

---
+++ GeometryChallenge/GeometryChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryChallengeApp.swift
+//  GeometryChallenge
+//
+//  Created by Víctor Ávila on 28/09/24.
+//
+
+import SwiftUI
+
+@main
+struct GeometryChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
