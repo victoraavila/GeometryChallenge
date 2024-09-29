c77c9d97f0369ae330ab369fd824e3783884ec00
Víctor Ávila <victoraavila2000@gmail.com>
2024-09-28 21:43:33 -0300

    Challenge 2 completed

--- GeometryChallenge/ContentView.swift
+++ GeometryChallenge/ContentView.swift
@@ -15,13 +15,14 @@ struct ContentView: View {
             ScrollView(.vertical) {
                 ForEach(0..<50) { index in
                     GeometryReader { proxy in
-                        Text("Row #\(index)")
-                            .font(.title)
-                            .frame(maxWidth: .infinity)
-                            .background(colors[index % 7])
-                            .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
-                            .opacity(proxy.frame(in: .global).minY / 200)
-                    }
+                            Text("Row #\(index)")
+                                .font(.title)
+                                .frame(maxWidth: .infinity)
+                                .background(colors[index % 7])
+                                .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                                .opacity(proxy.frame(in: .global).minY / 200)
+                                .scaleEffect(max(0.000651 * proxy.frame(in: .global).maxY + 0.434, 0.5))
+                        }
                     .frame(height: 40)
                 }
             }
